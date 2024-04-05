9fac0ef18b8a0ad254d1ba3c0968e2ac294da02c
cobyg <cobyg@COBYS_LAPTOP>
2024-04-04 17:33:19 -0700

    Add project files.

---
+++ Hello_World/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //////Console.WriteLine("Welcome world");
+            //////Console.WriteLine("How is your day?");
+            //////Console.Write("How are you?");
+            ///
+            /*
+             * View / Git Changes
+             * Check readmen
+             * uncheck private repo
+             * create and push
+             * 
+             * browse on web
+             * 
+             * message
+             * commit and push
+             */
+        }
+
+
+    }
+   
+}
